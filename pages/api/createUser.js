4c8b1b3a3d9a50fdc7b30b1b3a0a27b2817b7ca1
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-05-23 20:49:27 +0530

    chore: without custom claims

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -3,20 +3,21 @@ import admin from '@/utils/firebaseAdmin';
 const createUser = async (req, res) => {
   const { email, password, displayName } = req.body.input.credentials;
 
-  console.log(admin);
   const user = await admin.auth().createUser({
     email: email,
     password: password,
     displayName: displayName,
   });
 
-  await admin.auth().setCustomUserClaims(user.uid, {
-    'https://hasura.io/jwt/claims': {
-      'x-hasura-allowed-roles': ['user'],
-      'x-hasura-default-role': 'user',
-      'x-hasura-user-id': user.uid,
-    },
-  });
+  // await admin.auth().setCustomUserClaims(user.uid, {
+  //   'https://hasura.io/jwt/claims': {
+  //     'x-hasura-allowed-roles': ['user'],
+  //     'x-hasura-default-role': 'user',
+  //     'x-hasura-user-id': user.uid,
+  //   },
+  // });
+
+  console.log(user);
 
   res
     .status(200)
