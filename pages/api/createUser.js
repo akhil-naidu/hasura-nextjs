8f52b9476f2062a63b2a997924b5fb0146377f96
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-05-23 19:52:07 +0530

    init: firebase admin to next functions

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,6 +1,20 @@
-const createUser = (req, res) => {
+import admin from '@/utils/firebaseAdmin';
+
+const createUser = async (req, res) => {
   const { email, password, displayName } = req.body.input.credentials;
-  res.status(200).json({ id: 2, email: email, displayName: displayName });
+
+  const user = admin.auth().createUser(email, password, displayName);
+  await admin.auth().setCustomUserClaims(user.uid, {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-allowed-roles': ['user'],
+      'x-hasura-default-role': 'user',
+      'x-hasura-user-id': user.uid,
+    },
+  });
+
+  res
+    .status(200)
+    .json({ id: user.uid, email: user.email, displayName: user.displayName });
 };
 
 export default createUser;
