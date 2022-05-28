b4deee2652006067325d734267d8c71545898f7a
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-05-28 21:03:24 +0530

    init: tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
+import '../styles/globals.css';
 import { AuthProvider } from '@/utils/context/AuthContext';
 import { Provider } from 'urql';
 import { client } from '@/utils/urqlClient';
