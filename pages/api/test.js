71b0ca3c0ade1b0d3f869cd24b3e61e29bb2153e
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-07-15 18:09:22 +0530

    chore: continued

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,4 +1,9 @@
 export default function handler(req, res) {
-  console.log(req.body.id);
-  res.status(200).json({ friend: req.body.id });
+  if (req.method !== 'POST') {
+    const body = JSON.parse(req.body);
+    console.log('without parsing', req.body);
+    console.log('with parsing', body);
+    res.status(200).json({ friend: req.body.id ?? body.id });
+    return;
+  }
 }
