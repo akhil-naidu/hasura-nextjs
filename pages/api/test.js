8c48787955a6d8812b4eff4222a3194a226aaeca
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-07-15 18:14:38 +0530

    chorr: test

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,9 +1,8 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    const body = JSON.parse(req.body);
     console.log('without parsing', req.body);
-    console.log('with parsing', body);
-    res.status(200).json({ friend: req.body.id ?? body.id });
+
+    res.status(200).json({ friend: req.body.id });
     return;
   }
 }
