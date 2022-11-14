ebefc79559f8eea02e54a65e76ef9717aeeb5a6b
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-11-14 18:13:13 +0530

    chore: quick update for cache demo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Akhil Naidu' });
+  const date = new Date();
+  res.status(200).json({ timeOfPing: `${date}` });
 }
